d1456ac18b744433642a1d43c11ba8922291808c
Akira Designs <86624211+StrangerThigss@users.noreply.github.com>
2022-04-29 15:44:34 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// scroll suave 
+$(document).ready(function () {
+
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+
+            var hash = this.hash;
+
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 600, function () {
+
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+//animações scroll
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+};
+
+const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
+
+const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+        } else if (elementOutofView(el)) {
+            hideScrollElement(el)
+        }
+    })
+}
+
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+});
+
+//voltar ao topo
+var buttontop = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        buttontop.addClass('show');
+    } else {
+        buttontop.removeClass('show');
+    }
+});
+
+buttontop.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
